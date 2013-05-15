a51821b7fea9453420c4b95dfbf05ccab00d5264
Tasos Laskos <tasos.laskos@gmail.com>
2013-05-16 00:08:11 +0300

    Server::Handler: Added comments

--- lib/arachni/rpc/em/server/handler.rb
+++ lib/arachni/rpc/em/server/handler.rb
@@ -11,9 +11,10 @@ module RPC::EM
 class Server
 
 #
-# Server connection handler.
+# Receives `Arachni::RPC::Request` objects and transmits `Arachni::RPC::Response`
+# objects.
 #
-# @author: Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
 class Handler < EventMachine::Connection
     include Protocol
@@ -22,73 +23,77 @@ class Handler < EventMachine::Connection
 
     #INACTIVITY_TIMEOUT = 10
 
+    # @return   [Arachni::RPC::Request] Working RPC request.
     attr_reader :request
 
+    # @param    [Server]    server  RPC server.
     def initialize( server )
         super
-        @server = server
-        @opts   = server.opts.dup
+
+        @server  = server
+        @opts    = server.opts.dup
+        @request = nil
 
         assume_server_role!
-
-        @id = nil
-        @request = nil
 
         # Do not tolerate long periods of inactivity in order to avoid zombie
         # connections.
         #set_comm_inactivity_timeout( INACTIVITY_TIMEOUT )
     end
 
-    # starts TLS
+    # Initializes an SSL session once the connection has been established.
+    #
+    # @private
     def post_init
         start_ssl
     end
 
+    # Handles closed connections and cleans up the SSL session.
+    #
+    # @private
     def unbind
         end_ssl
         @server = nil
         close_connection
     end
 
-    def log( severity, progname, msg )
-        sev_sym = Logger.const_get( severity.to_s.upcase.to_sym )
-        @server.logger.add( sev_sym, msg, progname )
-    end
-
     #
-    # Handles requests and sends back the responses.
+    # * Handles a `Arachni::RPC::Request`
+    # * Sets the {#request}.
+    # * Sends back a `Arachni::RPC::Response`.
     #
     # @param    [Arachni::RPC::EM::Request]     req
     #
     def receive_request( req )
         @request = req
 
-        # the method call may block a little so tell EventMachine to
-        # stick it in its own thread.
+        # Create an empty response to be filled in little by little.
         res  = ::Arachni::RPC::Response.new
         peer = peer_ip_addr
 
         begin
-            # token-based authentication
+            # Make sure the client is allowed to make RPC calls.
             authenticate!
 
-            # grab the result of the method call
+            # Grab the partially filled in response which includes the result
+            # of the RPC call and merge it with out prepared response.
             res.merge!( @server.call( self ) )
 
-                # handle exceptions and convert them to a simple hash,
-                # ready to be passed to the client.
+        # Handle exceptions and convert them to a simple hash, ready to be
+        # passed to the client.
         rescue Exception => e
-
             type = ''
 
-            # if it's an RPC exception pass the type along as is
+            # If it's an RPC exception pass the type along as is...
             if e.rpc_exception?
                 type = e.class.name.split( ':' )[-1]
-                # otherwise set it to a RemoteExeption
+
+            # ...otherwise set it to a RemoteException.
             else
                 type = 'RemoteException'
             end
 
+            # RPC conventions for exception transmission.
             res.obj = {
                 'exception' => e.to_s,
                 'backtrace' => e.backtrace,
@@ -99,12 +104,27 @@ class Handler < EventMachine::Connection
             @server.logger.error( 'Exception' ){ msg + " [on behalf of #{peer}]" }
         end
 
-        #
-        # pass the result of the RPC call back to the client
-        # along with the callback ID but *only* if it wan't async
-        # because server.call() will have already taken care of it
-        #
+        # Pass the result of the RPC call back to the client but *only* if it
+        # wasn't async, otherwise {Server#call} will have already taken care of it
         send_response( res ) if !res.async?
+    end
+
+    private
+
+    # @param    [Symbol]    severity    Severity of the logged event:
+    #   * `:debug`
+    #   * `:info`
+    #   * `:warn`
+    #   * `:error`
+    #   * `:fatal`
+    #   * `:unknown`
+    #
+    # @param    [String]    category    Category of message (SSL, Call, etc.).
+    # @param    [String]    msg Message to log.
+    #
+    def log( severity, category, msg )
+        sev_sym = Logger.const_get( severity.to_s.upcase.to_sym )
+        @server.logger.add( sev_sym, msg, category )
     end
 
     #
